0f233bf38de8acb232b303395ed2f8246d2057eb
Nafisa Axmed <nafisa@Nafisas-MacBook-Air.local>
2021-04-09 12:58:51 +0100

    added text to speech functions

--- index.js
+++ index.js
@@ -7,6 +7,38 @@ const stopBUtton = document.querySelector("#stop");
 
 const setSpeechText = (text) => (speechObj.text = text);
 
+function populateVoices(){
+    voices = this.getVoices() // this = target of event
+    voicesDropdown.innerHTML = voices
+    .map(voice => `<option value= "${voice.name}">${voice.name} (${voice.lang})</option>`)
+     .join(" ")
+    
+}
+
+function setVoice(){
+   speechObj.voice = voices.find(voice => voice.name === this.value)
+   toggle();  
+}
+
+function toggle(startOver = true){
+    speechSynthesis.cancel();
+    if (startOver){
+        speechSynthesis.speak(speechObj)
+    }
+
+}
+
+function setOption(){
+    speechObj[this.name] = this.value;
+   toggle()
+}
 
 setSpeechText("Hi Nafisa and Antonio");
 
+speechSynthesis.addEventListener("voiceschanged", populateVoices);
+voicesDropdown.addEventListener("change", setVoice);
+options.forEach(option => option.addEventListener("change", setOption))
+speakButton.addEventListener("click", toggle)
+stopBUtton.addEventListener("click", function(){
+    toggle(false);
+});
